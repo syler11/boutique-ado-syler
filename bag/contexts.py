79563742450895158bbd588b066135a226916fdc
syler11 <nemeth.szilard82@gmail.com>
2022-03-17 19:16:56 +0000

    Sorted the glitch

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,27 +20,27 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-        else: 
+        else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
-                     'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-                'size': size,
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else: 
+    else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag contents """
-    return render(request, 'bag/bag.html')
+    """ A view that renders the bag contents page """
 
+    return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
