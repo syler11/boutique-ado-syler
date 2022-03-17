e6c67a2f74b95e9bae4d330dc25e2cfebb38651b
syler11 <nemeth.szilard82@gmail.com>
2022-03-17 16:23:40 +0000

    addded quantity to different sizes

--- products/migrations/0002_auto_20220317_1440.py
+++ products/migrations/0002_auto_20220317_1440.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='product',
-            name='has_sizes',
+            name='has_sized',
             field=models.BooleanField(blank=True, default=False, null=True),
         ),
     ]
