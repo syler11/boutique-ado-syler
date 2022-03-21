fc68d04b05591b03334a98c78b390921a2b44924
syler11 <nemeth.szilard82@gmail.com>
2022-03-21 21:02:12 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KfrZZLWVxhhz4ldZddEBkU3ZWVI6MqrrFOHqN3Z31DLmywjLXFeRGjzW2vRVeqtVDHg7cF3DqKevUV8bFlCmW7H001UlCU1OB',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
