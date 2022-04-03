985810b9906c45bcd770a2b636f4d84f938182d6
syler11 <nemeth.szilard82@gmail.com>
2022-04-03 18:20:38 +0000

    Debig changed to false

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = FALSE 
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['syler-boutique-ado.herokuapp.com', 'localhost']
 
